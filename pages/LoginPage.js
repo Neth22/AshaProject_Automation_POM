cfe2682a0c957ad88531a8a14ba8b7ce170f4a34
Neth22 <nseneviratne44@gmail.com>
2026-09-18 14:20:02 +0530

    Add Playwright automation project

---
+++ pages/LoginPage.js
@@ -0,0 +1,11 @@
+class LoginPage {
+
+    constructor(page){
+        this.page = page;
+        //login url 
+       this.loginUrl= page.goto("https://asha-securities-web.innov8hrm.com/login");
+
+       
+
+    }
+}
