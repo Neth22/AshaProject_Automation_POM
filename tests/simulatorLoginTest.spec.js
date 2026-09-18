1aeec90267e2693012a4620293ecbd44a47457de
Neth22 <nseneviratne44@gmail.com>
2026-09-18 20:19:32 +0530

    *Verify the toggle password visibility when clicking eye icon
    Signed-off-by: Neth22 <nseneviratne44@gmail.com>

--- tests/simulatorLoginTest.spec.js
+++ tests/simulatorLoginTest.spec.js
@@ -2,16 +2,24 @@ import test, { expect } from "@playwright/test";
 import { SimulatorLogin } from "../pages/SimulatorLoginPage.js";
 
 test.describe("Simulator Login Test Cases", () => {
-    
-    let simulatorLogin;
+  let simulatorLogin;
 
-    test.beforeEach(async ({ page }) => {
-        simulatorLogin = new SimulatorLogin(page);
-        await simulatorLogin.goto();
-    });
+  test.beforeEach(async ({ page }) => {
+    simulatorLogin = new SimulatorLogin(page);
+    await simulatorLogin.goto();
+  });
 
-    test("SLGN_01: Should display all initial UI elements correctly", async () => {
-        await simulatorLogin.verifyLoginPage();
-      });
-    
-})
+  test("SLGN_01: Should display all initial UI elements correctly", async () => {
+    await simulatorLogin.verifyLoginPage();
+  });
+
+  test("SLGN 02 : Should toggle password visibility when clicking eye icon", async () => {
+    await simulatorLogin.typePassword("simulator@1234");
+
+    await expect(simulatorLogin.passwordInput).toHaveAttribute("type", "password");
+
+    await simulatorLogin.showPassword.click();
+
+    await simulatorLogin.hidePassword.click();
+  });
+});
