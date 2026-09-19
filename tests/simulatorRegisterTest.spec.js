36cbf1ed459b98e9bdd5804faed13dea68ffcad6
Neth22 <nseneviratne44@gmail.com>
2026-09-19 14:02:34 +0530

    *Verify toggle password visibility when clicking eye icon
    Signed-off-by: Neth22 <nseneviratne44@gmail.com>

--- tests/simulatorRegisterTest.spec.js
+++ tests/simulatorRegisterTest.spec.js
@@ -1,27 +1,28 @@
-import test from "@playwright/test";
+import test, { expect } from "@playwright/test";
 import { SimulatorRegisterPage } from "../pages/SimulatorRegisterPage.js";
 
 test.describe("Simulator Register Test Cases", () => {
-    let simulatorRegister;
+  let simulatorRegister;
 
-    test.beforeEach(async ({ page }) => {
-        simulatorRegister = new SimulatorRegisterPage(page);
-        await simulatorRegister.goto();
-    });
+  test.beforeEach(async ({ page }) => {
+    simulatorRegister = new SimulatorRegisterPage(page);
+    await simulatorRegister.goto();
+  });
 
-    test("SRGN_01: Should display all initial UI elements correctly", async () => {
-        await simulatorRegister.verifyRegisterPage();
-    });
-    
+  test("SRGN_01: Should display all initial UI elements correctly", async () => {
+    await simulatorRegister.verifyRegisterPage();
+  });
 
+  test("SRGN 02 : Should toggle password visibility when clicking eye icon", async () => {
+    await simulatorRegister.enterPassword("simulator@1234");
 
+    await expect(simulatorRegister.passwordInput).toHaveAttribute(
+      "type",
+      "password",
+    );
 
+    await simulatorRegister.eyeIconShow.click();
 
-
-
-
-
-
-
+    await simulatorRegister.eyeIconHide.click();
+  });
 });
-
